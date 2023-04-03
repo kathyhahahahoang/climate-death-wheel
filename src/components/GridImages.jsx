8fb0afe914df3c67a807c4e2d26515f44292c584
HahaKhoang <kathyhahoang@gmail.com>
2023-04-03 16:02:56 -0500

    Updates description and title in GridImages

--- src/components/GridImages.jsx
+++ src/components/GridImages.jsx
@@ -8,48 +8,43 @@ import permafrost from "../img/permafrost.jpg";
 import poorAirQuality from "../img/poor-air-quality.jpg";
 import risingSeaLevels from "../img/rising-sea-levels.jpg";
 import superstorm from "../img/superstorm.jpg";
+import WheelOptions from "../constants/WheelOptions";
 import { useState } from "react";
+import { type } from "@testing-library/user-event/dist/type";
 
 const Options = () => {
   const [showTiles, setShowTiles] = useState(true);
   const [showLarge, setShowLarge] = useState(false);
+  const [imageId, setImageId] = useState();
+  const [title, setTitle] = useState("Capitalism");
+  const [description, setDescription] = useState(
+    "The world’s richest people emit huge and unsustainable amounts of carbon and, unlike ordinary people, 50% to 70% of their emissions result from their investments. 125 of the world’s richest billionaires show that on average they emit 3 million tonnes a year, more than a million times the average for someone in the bottom 90% of humanity. Billionaires hold extensive stakes in many of the world’s largest and most powerful corporations, which gives them the power to influence the way these companies act. The role of the super-rich in super-charging climate change is rarely discussed. These billionaire investors at the top of the corporate pyramid have huge responsibility for driving climate breakdown."
+  );
 
-  const changeHandler = () => {
+  const changeHandler = (event) => {
     setShowLarge(!showLarge);
     setShowTiles(!showTiles);
+    const id = event.target.id;
+    const image = WheelOptions.wheelOptions[id].image;
+    const header = WheelOptions.wheelOptions[id].title;
+    const text = WheelOptions.wheelOptions[id].text;
+    setImageId(image);
+    setTitle(header);
+    setDescription(text);
   };
 
   return (
     <div className={styles.container}>
       <div className={styles["left-container"]}>
-        <div className={styles.header}>Capitalism</div>
-        <div className={styles.description}>
-          The world’s richest people emit huge and unsustainable amounts of
-          carbon and, unlike ordinary people, 50% to 70% of their emissions
-          result from their investments. 125 of the world’s richest billionaires
-          show that on average they emit 3 million tonnes a year, more than a
-          million times the average for someone in the bottom 90% of humanity.
-          Billionaires had an average of 14 percent of their investments in
-          polluting industries such as fossil fuels and cement, double the
-          average for the Standard & Poor 500 group of companies. Billionaires
-          hold extensive stakes in many of the world’s largest and most powerful
-          corporations, which gives them the power to influence the way these
-          companies act. The role of the super-rich in super-charging climate
-          change is rarely discussed. This has to change. These billionaire
-          investors at the top of the corporate pyramid have huge responsibility
-          for driving climate breakdown. They have escaped accountability for
-          too long. Governments must hold them to account, legislating to compel
-          corporations and investors to reduce carbon emissions, enforcing more
-          stringent reporting requirements and imposing new taxation on wealth
-          and investments in polluting industries.
-        </div>
+        <div className={styles.header}>{title}</div>
+        <div className={styles.description}>{description}</div>
       </div>
       <div className={styles["right-container"]}>
         {showLarge && (
           <img
             className={styles.largePic}
             onClick={changeHandler}
-            src={poorAirQuality}
+            src={imageId}
           ></img>
         )}
 
@@ -59,6 +54,7 @@ const Options = () => {
               className={styles.pic}
               onClick={changeHandler}
               src={heatwave}
+              id="23"
             ></img>
           </div>
         )}
@@ -68,6 +64,7 @@ const Options = () => {
               className={styles.pic}
               onClick={changeHandler}
               src={poverty}
+              id="0"
             ></img>
           </div>
         )}
@@ -77,6 +74,7 @@ const Options = () => {
               className={styles.pic}
               onClick={changeHandler}
               src={disease}
+              id="30"
             ></img>
           </div>
         )}
@@ -86,6 +84,7 @@ const Options = () => {
               className={styles.pic}
               onClick={changeHandler}
               src={drought}
+              id="6"
             ></img>
           </div>
         )}
@@ -95,6 +94,7 @@ const Options = () => {
               className={styles.pic}
               onClick={changeHandler}
               src={capitalism}
+              id="3"
             ></img>
           </div>
         )}
@@ -104,6 +104,7 @@ const Options = () => {
               className={styles.pic}
               onClick={changeHandler}
               src={permafrost}
+              id="17"
             ></img>
           </div>
         )}
@@ -113,6 +114,7 @@ const Options = () => {
               className={styles.pic}
               onClick={changeHandler}
               src={poorAirQuality}
+              id="12"
             ></img>
           </div>
         )}
@@ -122,6 +124,7 @@ const Options = () => {
               className={styles.pic}
               onClick={changeHandler}
               src={risingSeaLevels}
+              id="42"
             ></img>
           </div>
         )}
@@ -131,6 +134,7 @@ const Options = () => {
               className={styles.pic}
               onClick={changeHandler}
               src={superstorm}
+              id="36"
             ></img>
           </div>
         )}
